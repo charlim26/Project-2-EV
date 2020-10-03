4b1196bacb024d70eec0f7c5a62294aae2b3b572
Jay Sueno <jay.sueno@gmail.com>
2020-10-03 10:46:28 -0700

    working on developing flask app integration with html and .js file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // need to create a simple viz using d3.json(`/whatever_route`).then(function(something) {})
+d3.select(".bar-plot").text("hello world")
+
 
 d3.json(`http://localhost:5000/electric`).then(function(data) {
     console.log(data);
