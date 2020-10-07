7e9604002bb859c240b25a5a4dc4c644c4673290
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-10-06 18:38:06 -0700

    createmap(data){}

--- static/app.js
+++ static/app.js
@@ -1,18 +1,9 @@
-// need to create a simple viz using d3.json(`/whatever_route`).then(function(something) {})
-// let allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', "*");
-//     res.header('Access-Control-Allow-Headers', "*");
-//     next();
-//   }
-//app.use(allowCrossDomain);
-
-
-
 d3.select("#bar-plot").text("hello world")
 
 var test = [1,2,3]
 console.log("start");
-fetch(`http://127.0.0.1:5000/electric`, {mode: "no-cors"})
+
+fetch(`/electric`, {mode: "no-cors"})
     .then(function(response) {
         // console.log(response);
         var r = response.json();
@@ -21,13 +12,23 @@ fetch(`http://127.0.0.1:5000/electric`, {mode: "no-cors"})
         return r;
     }).then(function(data){
         // console.log("data:");
-        // console.log(data);
+        console.log(data);
+        createMap(data);
 });
+
 console.log("middle");
+
 fetch(`/stations`, {mode: "no-cors"})
     .then(function(response) {
         return response.json();
     }).then(function(data){
         console.log(data);
+        
+        createMap(data);
 });
+
 console.log("end");
+
+function createMap (data) {
+
+}
