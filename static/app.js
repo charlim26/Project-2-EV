0920e47a6bd755224a97b96b307eb3f32e59042f
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-10-06 20:57:50 -0700

    what are you DOING leaflet?

--- static/app.js
+++ static/app.js
@@ -4,7 +4,6 @@
 var test = [1,2,3]
 console.log("start");
 
-var map = createMap();
 
 fetch(`/electric`, {mode: "no-cors"})
     .then(function(response) {
@@ -32,6 +31,38 @@ fetch(`/stations`, {mode: "no-cors"})
 
 console.log("end");
 
+
+
+// Define streetmap and darkmap layers
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+});
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+});
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+};
+// Create overlay object to hold our overlay layer
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load
+var myMap = L.map("map", {
+    center: [
+    47.61, -122.33
+    ],
+    zoom: 10,
+    layers: [streetmap]
+});
 
 
 
