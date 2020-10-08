2457cbb85d67db9bac295adf85569d55775b6fe4
Nghia Nguyen <nghianguyen214@gmail.com>
2020-10-07 19:55:41 -0700

    trying to add layers

--- static/app.js
+++ static/app.js
@@ -17,21 +17,37 @@ fetch(`/electric`, {mode: "no-cors"})
         // console.log("response:");
         // console.log(r);
         return r;
-    }).then(function(data){
+    }).then(function(car_data){
         // console.log("data:");
-        console.log(data);
-        createMap(data);
+        console.log(car_data[0]);
+        passCarData(car_data);
 });
 console.log("middle");
-fetch(`/stations`, {mode: "no-cors"})
-    .then(function(response) {
-        return response.json();
-    }).then(function(stations){
-        console.log(stations);
-        // createMap(stations);
-});
+function passCarData(car_data) {
+    fetch(`/stations`, {mode: "no-cors"})
+        .then(function(response) {
+            return response.json();
+        }).then(function(station_data){
+            console.log(station_data[0]);
+            passStationData(car_data, station_data);
+    })
+};
+function passStationData(car_data, station_data) {
+
+    d3.json(`static/WA_counties_geo.json`, function(geo) {
+        function onEachFeature(feature, layer) {
+            layer.bindPopup("<h3>" + feature.properties.NAME + "</h3>");
+        }
+        console.log(geo.features[0]);
+        var geolayer = L.geoJSON(geo.features, {
+            onEachFeature: onEachFeature
+        });
+        createMap(car_data, station_data, geolayer);
+    })
+}
+
 console.log("end");
-function createMap(data) {
+function createMap(car_data, station_data, geo) {
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -55,9 +71,24 @@ function createMap(data) {
         "Dark Map": darkmap
     };
     
+    // An array which will be used to store created stationMarkers
+    var stationMarkers = [];
+    
+    for (var i = 0; i < station_data.length; i++) {
+        // loop through the station array, create a new marker, push it to the stationMarkers array
+        stationMarkers.push(
+        L.marker(station_data[i].lat, station_data[i].long).bindPopup("<h1>" + station_data[i].address + "</h1> <hr> <h3>Number of outlets " + station_data[i].outlets + "</h3> <hr> <h3>Operational status " + station_data[i].status + "</h3>")
+        );
+    }
+    
+    // Add all the stationMarkers to a new layer group.
+    // Now we can handle them as one group instead of referencing each individually
+    var stationLayer = L.layerGroup(stationMarkers);
+
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
-        data: data
+        counties: geo,
+        stations: stationLayer
     };
     
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -66,20 +97,10 @@ function createMap(data) {
         47.88, -120.64
         ],
         zoom: 7,
-        layers: [streetmap, data]
+        layers: [streetmap, geo, stationLayer]
     });
-    
+
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
       }).addTo(myMap);
-
-    for (var i = 0; i < data.make; i++) {
-        console.log(data.make);
-
-    // var make = data[i]; // here we are assigning a variable to an object within the array, as we iterate through, city will be ONE object of the array and move onto the next as we iterate through
-    // L.marker(data.vehicle_location) // we grab the value within that object's key - here we are grabbing the lat and long values from the key, location
-    //   .bindPopup("<h1>" + data.make + "</h1> <hr>")
-    //   .addTo(myMap);
-    }
-
 }
