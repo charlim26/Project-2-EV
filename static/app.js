85b4fdbdb4b4a31edddee580a395986e160d7ad2
Nghia Nguyen <nghianguyen214@gmail.com>
2020-10-07 19:09:59 -0700

    added changes to nghia's branch

--- static/app.js
+++ static/app.js
@@ -1,14 +1,14 @@
 // need to create a simple viz using d3.json(`/whatever_route`).then(function(something) {})
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "*");
-    next();
-  }
-//app.use(allowCrossDomain);
+// let allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Headers', "*");
+//     next();
+//   }
+// //app.use(allowCrossDomain);
 
-d3.select("#bar-plot").text("hello world");
+// d3.select("#bar-plot").text("hello world");
 
-var test = [1,2,3]
+// var test = [1,2,3]
 
 fetch(`/electric`, {mode: "no-cors"})
     .then(function(response) {
@@ -26,9 +26,9 @@ console.log("middle");
 fetch(`/stations`, {mode: "no-cors"})
     .then(function(response) {
         return response.json();
-    }).then(function(data){
-        console.log(data);
-        // createMap(data);
+    }).then(function(stations){
+        console.log(stations);
+        // createMap(stations);
 });
 console.log("end");
 function createMap(data) {
@@ -63,13 +63,23 @@ function createMap(data) {
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
         center: [
-        47.61, -122.33
+        47.88, -120.64
         ],
-        zoom: 5,
+        zoom: 7,
         layers: [streetmap, data]
     });
     
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
       }).addTo(myMap);
+
+    for (var i = 0; i < data.make; i++) {
+        console.log(data.make);
+
+    // var make = data[i]; // here we are assigning a variable to an object within the array, as we iterate through, city will be ONE object of the array and move onto the next as we iterate through
+    // L.marker(data.vehicle_location) // we grab the value within that object's key - here we are grabbing the lat and long values from the key, location
+    //   .bindPopup("<h1>" + data.make + "</h1> <hr>")
+    //   .addTo(myMap);
+    }
+
 }
